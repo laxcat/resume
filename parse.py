50e307e4c90ca90090c87447952ff384541cb385
Tyler Martin <tyler@tylermartin.net>
2024-10-10 10:12:24 -0600

    updates, parser update

--- parse.py
+++ parse.py
@@ -9,8 +9,18 @@ import pprint
 DIR = dirname(abspath(__file__))
 RUN = lambda cmd, path=DIR : run(cmd, shell=True, cwd=path, capture_output=True)
 
+# IN-OUT SETTINGS
+in_file         = "tyler-martin-resume.txt"
+out_file        = "tyler-martin-resume.pdf"
+# STARTING SETTINGS
+margin_size     = .7
+font_size       = 11
+max_attempts    = 10
+runt_threshold  = 3
+
+
 def main():
-    s = RUN("pandoc -t json tyler-martin-resume.txt").stdout
+    s = RUN(f"pandoc -t json {in_file}").stdout
     j = json.loads(s)
     b = j["blocks"]
 
@@ -20,22 +30,11 @@ def main():
     exp     = parseExp(b, 6)
     edu     = parseEdu( b[15]["c"])
 
-    # makePDF(
-    #     margin_size = f"{margin_size}in",
-    #     font_size   = f"{font_size}pt",
-    #     info        = info,
-    #     summary     = summary,
-    #     skills      = skills,
-    #     exp         = exp,
-    #     edu         = edu,
-    # )
+    global margin_size
+    global font_size
 
-    margin_size = 1
-    font_size = 11
-    i = 1
-    while True:
-        print("ATTEMPT", i, margin_size, font_size)
-        page_count, out_pdf = makePDF(
+    def make():
+        return makePDF(
             margin_size = f"{margin_size}in",
             font_size   = f"{font_size}pt",
             info        = info,
@@ -44,15 +43,25 @@ def main():
             exp         = exp,
             edu         = edu,
         )
-        if page_count > 2 and i < 5:
-            print("TOO MANY PAGES!")
-            margin_size *= .95
-            font_size *= .955
-            i += 1
+
+    attempt = 1
+    while True:
+        print("ATTEMPT", attempt, margin_size, font_size)
+        res = make()
+        if res["page_count"] > 2 and attempt <= max_attempts:
+            print(f"TOO MANY PAGES ({res['page_count']})!")
+            margin_size *= .90
+            font_size *= .98
+            attempt += 1
             continue
         break
 
-    RUN(f"open {out_pdf}")
+    finish_str = f"({attempt} attempts), margin:{margin_size}in, font:{font_size}"
+    if res["page_count"] <= 2:
+        print(f"SUCCESS", finish_str)
+        RUN(f"open {out_file}")
+    else:
+        print(f"FAILED", finish_str)
 
 def gatherUntil(a, i, until="LineBreak"):
     while i < len(a) and a[i]["t"] != "Str":
@@ -136,17 +145,23 @@ def parseEdu(edu):
 def parseList(summary):
     return [joinNodes(x[0]["c"]) for x in summary]
 
+def addNBSPs(x):
+    items = x.split(" ")
+    n = runt_threshold - 1
+    nbsp = "&nbsp;"
+    return " ".join(items[:-n]) + nbsp + nbsp.join(items[-n:])
+
+def makeListItem(items, join_str):
+    item = '<span class="summary"><span>•</span><span>{x}</span></span>'
+    return join_str.join([item.format(x=addNBSPs(x)) for x in items])
+
 def makePDF(font_size="11pt", margin_size="1in", info={}, summary=[], skills=[], exp=[], edu={}):
     tab = "    "
-    indent = f"\n{tab}{tab}"
-    indent2 = f"\n{tab}"
-    brIndent = f"<br>\n{tab}"
-    item = '<span class="summary"><span>•</span><span>{x}</span></span>'
-    summaryLIs = indent.join([item.format(x=x) for x in summary])
-    skillsLIs = indent.join([item.format(x=x) for x in skills])
+    summaryItems = makeListItem(summary, f"\n{tab}{tab}")
+    skillsItems = makeListItem(skills, f"\n{tab}{tab}")
     experienceList = ""
     for e in exp:
-        sumList = f"\n{tab}{tab}{tab}".join([item.format(x=x) for x in e["summary"]])
+        sumList = makeListItem(e["summary"], f"\n{tab}{tab}{tab}")
         experienceList += f"""<p class="exp">
             <span class="dates">{e["dates"]}</span>
             <span class="company">Company: {e["company"]}</span>
@@ -164,19 +179,22 @@ def makePDF(font_size="11pt", margin_size="1in", info={}, summary=[], skills=[],
     @media print {{
         @page {{
             size: letter;
-            margin: {margin_size};
+            margin: {margin_size} 1.05in {margin_size} 1.1in;
         }}
         * {{
             font-family: Times New Roman, serif;
             letter-spacing: .01em;
+            line-height: 1.22;
         }}
         h1 {{
             margin: 0;
+            margin-bottom: 2pt;
             font-size: 20pt;
             font-weight: bold;
         }}
         h2 {{
             margin: 0;
+            margin-bottom: 5pt;
             font-size: 13pt;
             font-weight: bold;
         }}
@@ -202,7 +220,10 @@ def makePDF(font_size="11pt", margin_size="1in", info={}, summary=[], skills=[],
         }}
         .exp > .title {{
             font-weight: bold;
-            margin-bottom: 0pt;
+            margin-bottom: 2pt;
+        }}
+        .exp > .dates {{
+            font-style: italic;
         }}
         .exp {{
             margin-bottom: 15pt;
@@ -211,6 +232,7 @@ def makePDF(font_size="11pt", margin_size="1in", info={}, summary=[], skills=[],
             display: grid;
             grid-template-columns: 10pt 1fr;
             margin-left: 2pt;
+            margin-bottom: 2pt;
         }}
         .summary > span {{
             display: block;
@@ -230,11 +252,11 @@ def makePDF(font_size="11pt", margin_size="1in", info={}, summary=[], skills=[],
     </section>
     <section id="summary">
         <h3>Summary</h3>
-        {summaryLIs}
+        {summaryItems}
     </section>
     <section id="skills">
         <h3>Skills</h3>
-        {skillsLIs}
+        {skillsItems}
     </section>
     <section id="exp">
         <h3>Experience</h3>
@@ -250,68 +272,27 @@ def makePDF(font_size="11pt", margin_size="1in", info={}, summary=[], skills=[],
 </html>
     """
     tmp_html = "/tmp/out.html"
-    out_pdf  = "resume.pdf"
     try:
         remove(tmp_html)
     except OSError:
         pass
     try:
-        remove(out_pdf)
+        remove(out_file)
     except OSError:
         pass
     with open(tmp_html, "w") as f:
         f.write(html)
 
-    cmd = f"weasyprint -v -d -p --hinting {tmp_html} {out_pdf}"
+    cmd = f"weasyprint -v -d -p --hinting {tmp_html} {out_file}"
     res = RUN(cmd)
-    out = res.stdout.decode("utf-8") + "\n" + res.stderr.decode("utf-8")
-    count = re.findall(r"Creating layout \- Page [0-9]+", out)
-    print(html)
-    print(cmd)
-    print(out)
-    return len(count), out_pdf
+    output = res.stdout.decode("utf-8") + "\n" + res.stderr.decode("utf-8")
+    page_count = len(re.findall(r"Creating layout \- Page [0-9]+", output))
+
+    return {
+        "html"          : html,
+        "cmd"           : cmd,
+        "output"        : output,
+        "page_count"    : page_count,
+    }
 
 main()
-
-
-        # .summary > span:nth-child(1) {{
-        #     margin: 0 10pt;
-        # }}
-        # .summary > span:nth-child(2) {{
-        #     text-indent: 22pt;
-        #     color: #00f;
-        # }}
-            # ul {{
-            #     margin: 0;
-            #     padding: 0;
-            #     padding-left: 15pt;
-            # }}
-            # li {{
-            #     margin: 0 0 -15pt 0;
-            #     padding: 0;
-            #     position: relative;
-            #     break-inside: avoid-page;
-            # }}
-            # li::marker {{
-            #     content: "";
-            # }}
-            # li span {{
-            #     display: block;
-            #     position: relative;
-            #     left: 15pt;
-            #     top: -15pt;
-            # }}
-            # p, li {{
-            #     font-size: {font_size};
-            #     line-height: 1.3;
-            # }}
-            # p.exp {{
-            #     margin-bottom: 5pt;
-            # }}
-            # p.exp span,
-            # p.exp b {{
-            #     display: block;
-            # }}
-            # ul.exp {{
-            #     margin-bottom: 11pt;
-            # }}
